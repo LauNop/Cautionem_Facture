fecd5beab8345ea7e38585d198550a15cfd39195
Laurent Nopoly <laurentnopoly@gmail.com>
2022-03-20 15:29:37 +0100

    Démarrage Intérraction : Boutton Connexion en cours de développement

--- app/src/main/java/com/example/cautionem/MainActivity.java
+++ app/src/main/java/com/example/cautionem/MainActivity.java
@@ -2,14 +2,31 @@ package com.example.cautionem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button connect,inscript;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        this.connect = (Button) findViewById(R.id.connectbut);
+        this.inscript = (Button) findViewById(R.id.inscriptbut);
+
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent otherActivity = new Intent(getApplicationContext(),com.example.cautionem.ui.login.LoginActivity.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
     }
 }
