a963bc83a3b75ae88510b7be903265e8c9934b0f
louis <louis.medeville@gmail.com>
2022-03-22 10:13:14 +0100

    Louis Update

---
+++ app/src/main/java/com/example/cautionem/CreationAssoActivity.java
@@ -0,0 +1,14 @@
+package com.example.cautionem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreationAssoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creation_asso);
+    }
+}
