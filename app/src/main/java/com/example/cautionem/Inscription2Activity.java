9a262daf6116c5756ca2fd87c1ecb538cbfd3f98
louis <louis.medeville@gmail.com>
2022-04-08 16:23:56 +0200

    Louis MegaUpdate

--- app/src/main/java/com/example/cautionem/Inscription2Activity.java
+++ app/src/main/java/com/example/cautionem/Inscription2Activity.java
@@ -2,13 +2,30 @@ package com.example.cautionem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Inscription2Activity extends AppCompatActivity {
+
+    private Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription2);
+
+        this.next = (Button) findViewById(R.id.nextbut);
+
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent otherActivity = new Intent(getApplicationContext(), AssoActivity.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
     }
 }
