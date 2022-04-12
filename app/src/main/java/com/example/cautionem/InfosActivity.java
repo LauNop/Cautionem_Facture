f65945de26a958608257e3ac68521549fb1ace09
louis <louis.medeville@gmail.com>
2022-04-12 11:23:22 +0200

    Update Louis 2

--- app/src/main/java/com/example/cautionem/InfosActivity.java
+++ app/src/main/java/com/example/cautionem/InfosActivity.java
@@ -2,13 +2,29 @@ package com.example.cautionem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InfosActivity extends AppCompatActivity {
+
+    private Button membre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infos);
+
+        this.membre = (Button) findViewById(R.id.nextbut4);
+
+        membre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Membre_Activity.class));
+                finish();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cautionem/SuiviActivity.java
+++ app/src/main/java/com/example/cautionem/SuiviActivity.java
@@ -2,13 +2,30 @@ package com.example.cautionem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SuiviActivity extends AppCompatActivity {
+
+    private Button info,devis,facture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suivi);
+
+        this.info = (Button) findViewById(R.id.nextbut6);
+
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),InfosActivity.class));
+                finish();
+
+            }
+        });
+
     }
 }
