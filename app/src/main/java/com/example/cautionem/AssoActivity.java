fa093ed15dd0816368dd20d12deba595776b50ef
Laurent Nopoly <laurentnopoly@gmail.com>
2022-04-19 17:58:52 +0200

    Mini AssoActivity Update

--- app/src/main/java/com/example/cautionem/AssoActivity.java
+++ app/src/main/java/com/example/cautionem/AssoActivity.java
@@ -5,6 +5,7 @@ import static com.example.cautionem.R.id.Asso_list;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -73,6 +74,13 @@ public class AssoActivity extends AppCompatActivity {
                                 AssoList.add(asso);
                                 ListView assoListView = findViewById(Asso_list);
                                 assoListView.setAdapter(new Asso_Adapter(AssoActivity.this, AssoList));
+
+                                assoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                    @Override
+                                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                        Toast.makeText(AssoActivity.this,"Vous cliquez sur l'association "+AssoList.get(i).getNom(),Toast.LENGTH_SHORT).show();
+                                    }
+                                });
                                 Toast.makeText(AssoActivity.this,document.getId() + " => " + document.getData(),Toast.LENGTH_SHORT).show();
                                 //Log.d(TAG, document.getId() + " => " + document.getData());
                             }
