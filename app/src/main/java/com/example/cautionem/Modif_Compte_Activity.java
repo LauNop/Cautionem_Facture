a5fde5c0a9117ab71276a43c1ccdfac9c28702c9
louis <louis.medeville@gmail.com>
2022-05-03 09:27:39 +0200

    Modif_compte Update

---
+++ app/src/main/java/com/example/cautionem/Modif_Compte_Activity.java
@@ -0,0 +1,14 @@
+package com.example.cautionem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Modif_Compte_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modif_compte2);
+    }
+}
